49bdc281b6ec7994a2723b1e2b0673cfffad3360
= <=>
2023-04-30 14:35:09 -0500

    add state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState([]);
+
   const fetchUsers = () => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((json) => {
+        setUser(json);
+      });
   };
 
   const fetchTodos = () => {
@@ -13,12 +18,21 @@ function App() {
       .then((response) => response.json())
       .then((json) => console.log(json));
   };
+
   return (
     <div className="App">
       <div className="topbar">
         <button onClick={fetchUsers}>User</button>
         <button onClick={fetchTodos}>Todos</button>
       </div>
+      {user.map((user, index) => {
+        return (
+          <div>
+            <h3>Name: {user.name}</h3>
+            <h3>User: {user.username}</h3>
+          </div>
+        );
+      })}
     </div>
   );
 }
